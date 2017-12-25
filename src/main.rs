7dc3d14a7e9ee6947364afc8a2098500e8880e84
Garrett Tacoronte <garretttacoronte@gmail.com>
2017-12-24 23:28:22 -0700

    Add rename logic with cli arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::env;
+use std::fs;
+use std::fs::DirEntry;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("config.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let text = matches.value_of("text").unwrap_or("");
+    let replace = matches.value_of("with").unwrap_or("");
+    let prepend = matches.value_of("prepend").unwrap_or("");
+    let cwd = env::current_dir().unwrap();
+    let paths = fs::read_dir(cwd).unwrap();
+    for path in paths {
+        let file_name: String = get_file_name(path.unwrap());
+        let replaced_name : String = str::replace(&file_name, text, replace);
+        let updated_name : String = prepend.to_string() + &replaced_name.to_string();
+        fs::rename(file_name, &updated_name);
+        println!("New file_name: {}", updated_name);
+    }
+}
+
+fn get_file_name(path: DirEntry) -> String {
+        let file_name : String = path
+            .file_name()
+            .into_string()
+            .expect("error: the first argument is not a file \
+                    system path representable in UTF-8.");
+        println!("Name: {:?}", file_name);
+        file_name
 }
